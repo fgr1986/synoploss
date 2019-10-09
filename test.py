added weight decay, threshold adjustment, fixed SynOps calculation

test.py
@@ -6,7 +6,7 @@ import numpy as np
 from tqdm import tqdm
 
 
-def test_spiking(path_to_weights):
+def test_spiking(path_to_weights, threshold=1.0):
     # instantiate dataloader
     test_dataset = AERFolderDataset(
         root='data/',
@@ -25,7 +25,13 @@ def test_spiking(path_to_weights):
     in_shape = (1, 64, 64)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    net = from_model(model, in_shape).to(device)
+    net = from_model(
+        model,
+        in_shape,
+        threshold=threshold,
+        membrane_subtract=threshold,
+        threshold_low=-1.0
+    ).to(device)
     net.spiking_model.eval()
 
     accuracy = []
@@ -37,35 +43,50 @@ def test_spiking(path_to_weights):
             # if i > 50: break
             test_data, test_labels = sample
             input_frames = test_data[0].to(device)
+            input_frames = input_frames.unsqueeze(1)
 
             # we reset the network when changing file
             net.reset_states()
 
             # loop over the 1 ms frames WITHIN a single input file
-            outputs = torch.cuda.FloatTensor([[0., 0.]])
-            for frame in input_frames:
-                frame = frame.unsqueeze(0).unsqueeze(0)
-                outputs += net.spiking_model(frame)
+            outputs = net.spiking_model(input_frames)
 
-                synops_df = net.get_synops(1)
-                synops.append(synops_df['SynOps'].mean())
+            synops_df = net.get_synops(3000)
+            synops.append(synops_df['SynOps'].sum())
 
             _, predicted = torch.max(outputs, 1)
             correctness = (predicted == test_labels.to(device))
-            accuracy.append(correctness.cpu().numpy())
+            accuracy.append(correctness.cpu().numpy().mean())
 
     return np.mean(accuracy), np.mean(synops)
+
+
+def test_varying_thres(threshold):
+    model = 'models/nopenalty_30_epochs.pt'
+    return test_spiking(model, threshold=threshold)
 
 
 if __name__ == '__main__':
     import glob
     from multiprocessing import Pool
 
+    # Use this for the whole list of trained model
     models_list = glob.glob('models/*')
-
-    P = Pool(4)
+    P = Pool(5)
     results = P.map(test_spiking, models_list)
     P.close()
     results = np.array(results).T
     results = np.vstack([models_list, results[0], results[1]]).T
+
+    # # Use below for variable threshold
+    # thresholds = np.arange(1, 4, 0.3)
+    # thr_descriptor = [f'nopen_thres_30_epochs_{thr}' for thr in thresholds]
+
+    # P = Pool(5)
+    # results = P.map(test_varying_thres, thresholds)
+    # P.close()
+    # results = np.array(results).T
+    # results = np.vstack([thr_descriptor, results[0], results[1]]).T
+
+    # Leave this there.
     np.savetxt('spk_results.txt', results, fmt='%s')

train.py
@@ -44,13 +44,15 @@ train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True
 test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 
-def train(activation_penalty=0., epochs=10, save=False):
+def train(penalty_coefficient=0., epochs=10, save=False):
     # Define model and learning parameters
     myclass = YundingClassifier().to(device)
     # Define loss
     criterion = torch.nn.CrossEntropyLoss()
     # Define optimizer
-    optimizer = torch.optim.Adam(myclass.parameters(), lr=1e-3)
+    decay_rate = penalty_coefficient if WEIGHT_DECAY else 0.
+    optimizer = torch.optim.Adam(myclass.parameters(), lr=1e-3,
+                                 weight_decay=decay_rate)
     # set hooks
     hookable_modules = ['seq.1', 'seq.4', 'seq.7', 'seq.12', 'seq.14']
 
@@ -94,7 +96,7 @@ def train(activation_penalty=0., epochs=10, save=False):
             accuracy_train.append(acc.cpu().numpy())
 
             target_loss = criterion(outputs, labels)
-            loss = target_loss + value_to_penalize * activation_penalty
+            loss = target_loss + value_to_penalize * penalty_coefficient
             running_loss += loss.item()
             loss.backward()
             optimizer.step()
@@ -130,11 +132,12 @@ def train(activation_penalty=0., epochs=10, save=False):
 
 if __name__ == '__main__':
     N_EPOCHS = 30
+    WEIGHT_DECAY = False
 
     # L2 neuron-wise
-    penalties = np.logspace(-3, 1, 10)
-    penalty_function = lambda out: (out.mean(0)**2).sum()
-    name = f"L2_neuronlevel_{N_EPOCHS}_epochs"
+    penalties = np.logspace(-2.8, -2.2, 10)
+    penalty_function = lambda out: 0.  # (out.mean(0)**2).sum()
+    name = f"weightdecay_{N_EPOCHS}_epochs"
     # # L2 layer-wise
     # penalties = np.logspace(-6, 0, 10)
     # penalty_function = lambda out: (out.mean(0).sum())**2
@@ -152,4 +155,4 @@ if __name__ == '__main__':
         train(p, N_EPOCHS, save=name + '_' + str(p))
     results = np.asarray([penalties, activation_values,
                           test_acc, target_losses]).T
-    np.savetxt(name + '.txt', results)
+    np.savetxt('results/' + name + '.txt', results)