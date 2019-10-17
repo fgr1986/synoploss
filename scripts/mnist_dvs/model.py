850db2890b9fd9c59ddbe7f10cb7e9eee5e85215
Martino Sorbaro <martino.sorbaro@posteo.net>
2019-10-17 16:15:38 +0200

    experiments with quantization

--- scripts/mnist_dvs/model.py
+++ scripts/mnist_dvs/model.py
@@ -1,26 +1,28 @@
 import torch
+from bp_quantize import NeuromorphicReLU
 
 
 class MNISTClassifier(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, quantize=False):
         super().__init__()
+
         self.seq = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=1, out_channels=8,
                             kernel_size=(3, 3), bias=False),
-            torch.nn.ReLU(),
+            NeuromorphicReLU(quantize=quantize, fanout=72),
             torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             torch.nn.Conv2d(in_channels=8, out_channels=12,
                             kernel_size=(3, 3), bias=False),
-            torch.nn.ReLU(),
+            NeuromorphicReLU(quantize=quantize, fanout=108),
             torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             torch.nn.Conv2d(in_channels=12, out_channels=12,
                             kernel_size=(3, 3), bias=False),
-            torch.nn.ReLU(),
+            NeuromorphicReLU(quantize=quantize, fanout=432),
             torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             torch.nn.Dropout2d(0.5),
             torch.nn.Flatten(),
             torch.nn.Linear(432, 10, bias=False),
-            torch.nn.ReLU(),
+            NeuromorphicReLU(quantize=quantize, fanout=10),
         )
 
     def forward(self, x):
---
+++ scripts/mnist_dvs/quantization_evaluation.py
@@ -0,0 +1,108 @@
+import torch
+from aer4manager import AERFolderDataset
+import torchvision.transforms as ttr
+from torch.utils.data import DataLoader
+from model import MNISTClassifier
+from tqdm import tqdm
+import numpy as np
+import argparse
+import uuid
+
+# ## LOAD ARGUMENTS ## #
+parser = argparse.ArgumentParser()
+parser.add_argument('--quantize_training', action='store_true', default=False)
+opt = parser.parse_args()
+print("Quantize training:", opt.quantize_training)
+
+# ## PREPARE DATASET ## #
+BATCH_SIZE = 512
+EPOCHS = 10
+NORM = 1.
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_dataset = AERFolderDataset(
+    root='data/train/',
+    from_spiketrain=False,
+    transform=ttr.ToTensor(),
+)
+
+test_dataset = AERFolderDataset(
+    root='data/test/',
+    from_spiketrain=False,
+    transform=ttr.ToTensor(),
+)
+
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+
+# ## DEFINE MODEL ## #
+classifier = MNISTClassifier(quantize=opt.quantize_training).to(device)
+# Define loss
+criterion = torch.nn.CrossEntropyLoss()
+# Define optimizer
+optimizer = torch.optim.Adam(classifier.parameters(), lr=3e-4)
+
+
+# ## TRAINING ## #
+for epoch in range(EPOCHS):
+    classifier.train()
+
+    print(f"Epoch {epoch}, training")
+    for batch_id, sample in enumerate(tqdm(train_dataloader)):
+        # if batch_id > 100: break
+        optimizer.zero_grad()
+
+        imgs, labels = sample
+        imgs, labels = imgs.to(device), labels.to(device)
+        imgs = imgs / NORM
+
+        outputs = classifier(imgs)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    print(f"Loss={loss.item()}, quantize_training={opt.quantize_training}")
+
+hash = uuid.uuid4().hex[0:6]
+path = f"quantization/{hash}.pth"
+torch.save(classifier.state_dict(), path)
+
+
+# ## TESTING ## #
+def evaluate(state_dict, quantize_testing):
+    with torch.no_grad():
+        accuracy = []
+
+        test_model = MNISTClassifier(quantize=quantize_testing).to(device)
+        test_model.load_state_dict(state_dict)
+        test_model.eval()
+
+        print("** TESTING **")
+        for batch_id, sample in enumerate(tqdm(test_dataloader)):
+            # if batch_id > 10: break
+            test_data, test_labels = sample
+            test_data = test_data.to(device)
+            test_data = test_data / NORM
+            test_labels = test_labels.to(device)
+
+            outputs = test_model(test_data)
+
+            _, predicted = torch.max(outputs, 1)
+            acc = (predicted == test_labels).sum().float() / len(test_labels)
+            accuracy.append(acc.cpu().numpy())
+
+        accuracy = np.mean(accuracy)
+
+    print(f"accuracy_test={accuracy}, quantize_testing={quantize_testing}")
+    return accuracy
+
+
+acc_quantized = evaluate(classifier.state_dict(), True)
+acc_analog = evaluate(classifier.state_dict(), False)
+
+with open("quantization/results.txt", "a") as f:
+    f.write(f"{hash}\t{opt.quantize_training}\tTrue\t{acc_quantized}\n")
+    f.write(f"{hash}\t{opt.quantize_training}\tFalse\t{acc_analog}\n")
