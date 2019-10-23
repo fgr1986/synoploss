21f720fd118f2bec3e11f471650b51707d90b1eb
Martino Sorbaro <martino.sorbaro@posteo.net>
2019-10-23 16:54:08 +0200

    corrected fanout

--- scripts/mnist_dvs/model.py
+++ scripts/mnist_dvs/model.py
@@ -9,7 +9,7 @@ class MNISTClassifier(torch.nn.Module):
         self.seq = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=1, out_channels=8,
                             kernel_size=(3, 3), bias=False),
-            NeuromorphicReLU(quantize=quantize, fanout=72),
+            NeuromorphicReLU(quantize=quantize, fanout=108),
             torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             torch.nn.Conv2d(in_channels=8, out_channels=12,
                             kernel_size=(3, 3), bias=False),
@@ -17,12 +17,12 @@ class MNISTClassifier(torch.nn.Module):
             torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             torch.nn.Conv2d(in_channels=12, out_channels=12,
                             kernel_size=(3, 3), bias=False),
-            NeuromorphicReLU(quantize=quantize, fanout=432),
+            NeuromorphicReLU(quantize=quantize, fanout=10),
             torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             torch.nn.Dropout2d(0.5),
             torch.nn.Flatten(),
             torch.nn.Linear(432, 10, bias=False),
-            NeuromorphicReLU(quantize=quantize, fanout=10),
+            NeuromorphicReLU(quantize=quantize, fanout=0),
         )
 
     def forward(self, x):
